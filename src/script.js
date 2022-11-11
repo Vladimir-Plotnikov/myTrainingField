18ead5b5fe36c77b665b20048ac3a6d739a7efb1
Volodymyr <vovdata@gmail.com>
2022-11-11 08:58:52 +0100

    LightBox
    1commit

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        while (lightbox.firstChild) {
+         lightbox.removeChild(lightbox.firstChild)   
+        }
+        img.src = image.src
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if(e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
